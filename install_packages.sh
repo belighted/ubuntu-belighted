5d052420dad983bd4c6f0870aff5b74d4f39c941
nicodax <nicodax@proton.me>
2023-03-10 14:41:09 +0100

    feat: Installing code and docker through snap (WIP)

--- install_packages.sh
+++ install_packages.sh
@@ -6,23 +6,12 @@ source variables
 ## PACKAGES
 sudo xargs -a packages.txt -r apt install -y -qq # install the packages listed on the packages.txt file
 sudo apt install linux-headers-$(uname -r) -y -qq
-## VSCODE
-mkdir .deb
-wget $CODE_DEB -O .deb/code.deb
-sudo apt install .deb/code.deb -y -qq
-## DOCKER
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL $DOCKER_UBUNTU_ROOT_URL/gpg | sudo gpg --dearmor -o $APT_KEYRING/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=$APT_KEYRING/docker.gpg] $DOCKER_UBUNTU_ROOT_URL $(lsb_release -cs) stable" | sudo tee $APT_SOURCE_LIST/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y -qq
-sudo groupadd docker
-sudo usermod -aG docker $USER
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
-sudo systemctl start docker.service
-sudo systemctl start containerd.service
-## SLACK
+## SNAPS
+### VISUAL STUDIO CODE
+sudo snap install code --classic
+### DOCKER
+sudo snap install docker
+### SLACK
 sudo snap install slack
 ## OFFICE PRINTER
 mkdir .tar
