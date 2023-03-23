42b403fadc741fdfa5b90bf4d1d22d5a7aa08511
nicodax <nicodax@proton.me>
2023-03-23 11:57:54 +0100

    fix: testing sleep.conf existence and fixed typo in sleep.conf file name

--- suspend_then_hibernate.sh
+++ suspend_then_hibernate.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 sudo bash -c "echo "HandleLidSwitch=suspend-then-hibernate" >> /etc/systemd/logind.conf"
-sudo cp ./sleep.conf /etc/systemd/sleepd.conf
+if test -f /etc/systemd/sleep.conf; then
+    sudo bash -c "echo "HibernateDelaySec=3600" >> /etc/systemd/sleep.conf"
+else
+    sudo cp sleep.conf /etc/systemd/sleep.conf
+fi
